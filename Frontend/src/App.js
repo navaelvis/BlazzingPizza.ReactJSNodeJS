0be6c87572b79e3d6566f69b17169ec75327953b
Elvis L. Nava <navaelvis@gmail.com>
2020-04-05 16:46:13 -0400

    menuButtons added

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import TopBar from './components/topBar/topBar';
 import Main from './components/main/main';
-import MyOrders from './components/orderList/myOrders';
+import MyOrders from './components/orderlist/myOrders';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
   return (
     <React.Fragment>
-      <TopBar />
-      <div className="content" >
         <Router>
-          <Route path="/" component={Main} exact ></Route>
-          <Route path="/myOrders" component={MyOrders} ></Route>
+          <TopBar />
+          <div className="content" >
+            <Route path="/" component={Main} exact ></Route>
+            <Route path="/myOrders" component={MyOrders} ></Route>
+          </div>
         </Router>
-        {/* <Main /> */}
-      </div>
     </React.Fragment>
   );
 }
--- Frontend/src/components/topBar/topBar.js
+++ Frontend/src/components/topBar/topBar.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import '../../css/site.css';
 import Logo from '../../images/logo.svg';
+import { NavLink } from 'react-router-dom';
 
 class TopBar extends React.Component {
     render() {
         return (
             <div className="top-bar">
                 <img className="logo" alt="Logo" src={Logo} />
+                <NavLink to="/" className="nav-tab" exact >
+                    <img src="/images/topbar/pizza-slice.svg" alt="Elegir Pizza" ></img>
+                    <div>Elegir Pizza</div>
+                </NavLink>
+                <NavLink to="/myOrders" className="nav-tab" exact >
+                    <img src="/images/topbar/bike.svg" alt="Mis ordenes" ></img>
+                    <div>Mis ordenes</div>
+                </NavLink>
             </div>
         );
     }
